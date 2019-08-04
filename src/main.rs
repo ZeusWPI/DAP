22f206e558bd2b01a10952b5dd01f66113b96658
Wout Schellaert <wout.schellaert@gmail.com>
2019-08-04 13:51:14 +0200

    Add an actix-web webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+const PORT: &str = "8088";
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Come here in the future to change your password.")
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Server soon to be running on http://127.0.0.1:{}", PORT);
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind(format!("127.0.0.1:{}", PORT))
+        .unwrap()
+        .run()
+        .unwrap();
 }
